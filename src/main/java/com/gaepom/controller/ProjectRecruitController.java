cb6c7b9ecfe6ba0d4278e50dbc3dea75190f37f5
ajdeve <ajdevlife@gmail.com>
2021-02-15 20:50:19 +0900

    Project Recruit & Project Controller and related classes
    프로젝트 리크루트와 프로젝트를 합치기

--- src/main/java/com/gaepom/controller/ProjectRecruitController.java
+++ src/main/java/com/gaepom/controller/ProjectRecruitController.java
@@ -1,113 +1,162 @@
 package com.gaepom.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.gaepom.domain.Project;
 import com.gaepom.domain.ProjectRecruit;
+import com.gaepom.domain.RequestWrapper;
 import com.gaepom.domain.User;
 import com.gaepom.service.ProjectRecruitService;
+import com.gaepom.service.ProjectService;
 
-@Controller
-@SessionAttributes("guser")
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping(value = "recruit")
 public class ProjectRecruitController {
 
 	@Autowired
 	private ProjectRecruitService projectRecruitService;
+
+	@Autowired
+	private ProjectService projectService;
 
 	@ModelAttribute("guser")
 	public User setUser() {
 		return new User();
 	}
 
-	@RequestMapping("/getProjectRecruitList")
-	public String getProjectRecruitList(@ModelAttribute("guser") User user, Model model,
-			ProjectRecruit recruit) {
-
-		if (user.getUserId() == null) {
-			return "redirect:login.html";
-		}
-
-		List<ProjectRecruit> projectRecruitList = projectRecruitService.getProjectRecruitList(recruit);
-
-		System.out.println(projectRecruitList);
-		model.addAttribute("projectRecruitList", projectRecruitList);
-		return "getProjectRecruitList";
-	}
-
-	@GetMapping("/insertProjectRecruit")
-	public String insertProjectRecruitView(@ModelAttribute("guser") User user) {
-		if (user.getUserId() == null) {
-			return "redirect:login";
-		}
-
-		return "insertProjectRecruit";
-	}
-
-	@PostMapping("/insertProjectRecruit")
-	public String insertProjectRecruit(@ModelAttribute("guser") User user, ProjectRecruit recruit) {
-		if (user.getUserId() == null) {
-			return "redirect:login";
-		}
-
-		projectRecruitService.insertProjectRecruit(recruit);
-		System.out.println("-------insertProjectRecruit----------------------");
-		return "redirect:getProjectRecruitList";
-	}
-
-	@GetMapping("/getProjectRecruit")
-	public String getProjectRecruit(@ModelAttribute("guser") User user, ProjectRecruit recruit, Model model) {
-		if (user.getUserId() == null) {
-			return "redirect:login";
-		}
-
-		model.addAttribute("projectRecruit", projectRecruitService.getProjectRecruit(recruit));
-		return "getProjectRecruit";
+	// 프로젝트 총 리스트 출력 (고쳐서 두개다 추력하게)
+	@GetMapping("/getall")
+	public ResponseEntity<List<Project>> findAllrecs(User user, @RequestBody RequestWrapper requestWrapper) {
+		List<Project> pj = projectService.getProjectList(requestWrapper.getProject());
+		List<ProjectRecruit> recs = projectRecruitService.getProjectRecruitList(requestWrapper.getRecruit());
+		return new ResponseEntity<>(pj, HttpStatus.OK);
 	}
 	
-	@GetMapping("/getPosition") // 필요포지션
-	public String getPosition(@ModelAttribute("guser") User user, ProjectRecruit recruit, Model model) {
-		if (user.getUserId() == null) {
-			return "redirect:login";
-		}
-		System.out.println(user.getUserId());
-		String[] needPosi = projectRecruitService.getPosition(recruit).split("\\,");
-		System.out.println(Arrays.asList(needPosi));
-		System.out.println(Arrays.asList(needPosi).get(0));
-		
-		List<ProjectRecruit> rec = projectRecruitService.getProjectRecruitList(recruit);
-		
-		model.addAttribute("rec", rec);
-		model.addAttribute("needPosi", Arrays.asList(needPosi)); //[개발자,  디자이너,  마케터 ]
+//	// 프로젝트 + 프로젝트 모집글 내용이 함께 상세페이지로 검색
+//	@GetMapping("/getall")
+//	public ResponseEntity<RequestWrapper> findAllrecs(User user, @RequestBody RequestWrapper requestWrapper) {
+//		List<Project> pj = projectService.getProjectList(requestWrapper.getProject());
+//		List<ProjectRecruit> recs = projectRecruitService.getProjectRecruitList(requestWrapper.getRecruit());
+//		return new ResponseEntity<>(pj, HttpStatus.OK);
+//	}
 
-		return "getPosition";
+
+	// 모집글 총 리스트 출력
+	@GetMapping("/getlist")
+	public ResponseEntity<List<ProjectRecruit>> findAllrecs(ProjectRecruit recruit) {
+		List<ProjectRecruit> recs = projectRecruitService.getProjectRecruitList(recruit);
+		return new ResponseEntity<>(recs, HttpStatus.OK);
 	}
 
-	@PostMapping("/updateProjectRecruit")
-	public String updateProjectRecruit(@ModelAttribute("guser") User user, ProjectRecruit recruit) {
+	// 프로젝트와 모집글 함께 생성
+	@PostMapping("create")
+	public ResponseEntity<Project> insertProjectRecruit(User user, @RequestBody RequestWrapper requestWrapper) {
 		if (user.getUserId() == null) {
-			return "redirect:login";
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+		System.out.println("-------insertRecruit--------");
+		requestWrapper.getProject().setRecSeq(projectRecruitService.insertProjectRecruit(requestWrapper.getRecruit()));
+		System.out.println("-------inserProject--------");
+		Project pj = projectService.insertProject(requestWrapper.getProject());
 
-		projectRecruitService.updateProjectRecruit(recruit);
-		return "forward:getProjectRecruitList";
+		return new ResponseEntity<>(pj, HttpStatus.CREATED);
 	}
 
-	@GetMapping("/deleteProjectRecruit")
-	public String deleteProjectRecruit(@ModelAttribute("guser") User user, ProjectRecruit recruit) {
+	// recSeq로 모집글 조회
+	@GetMapping("/get/{id}")
+	public ResponseEntity<ProjectRecruit> getRecruitByRecSeq(User user, @PathVariable("id") long id,
+			@RequestBody ProjectRecruit recruit) {
 		if (user.getUserId() == null) {
-			return "redirect:login";
+			System.out.println("실패1");
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+		try {
+			ProjectRecruit rec = projectRecruitService.getProjectRecruit(id);
+			System.out.println("성공");
+			return new ResponseEntity<>(rec, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
+	// pjSeq로 프로젝트만 불러오기 (Project Controller에서 하면 데이터를 많이 받아오므로 여기에 둠)
+	@GetMapping("/getpj/{id}")
+	public ResponseEntity<Project> getPjById(User user, @PathVariable("id") long id,
+			@RequestBody RequestWrapper requestWrapper) {
+		if (user.getUserId() == null) {
+			System.out.println("실패1");
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		try {
+			Project pj = projectService.getProject(id);
+			System.out.println("프로젝트 불러오기 성공");
+			return new ResponseEntity<>(pj, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	// 프로젝트 수정
+	@PutMapping("/updatepj/{id}")
+	public ResponseEntity<ProjectRecruit> updateRecruit(User user, @PathVariable("id") long id,
+			@RequestBody ProjectRecruit recruit) {
+		if (user.getUserId() == null) {
+			System.out.println("실패");
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		ProjectRecruit rec = projectRecruitService.updateProjectRecruit(id, recruit);
+		return new ResponseEntity<>(rec, HttpStatus.CREATED);
+	}
+
+	// 모집글수정
+	@PutMapping("/updaterec/{id}")
+	public ResponseEntity<ProjectRecruit> updateRec(User user, @PathVariable("id") long id,
+			@RequestBody ProjectRecruit recruit) {
+		if (user.getUserId() == null) {
+			System.out.println("실패");
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		ProjectRecruit rec = projectRecruitService.updateRec(id, recruit);
+		return new ResponseEntity<>(rec, HttpStatus.CREATED);
+
+	}
+
+	// 프로젝트 모집글 제거 (트래커 글이 삭제될때 같이 삭제되야 하므로 생각해보고 수정필요)
+	@DeleteMapping("/deleterec/{id}")
+	public ResponseEntity<HttpStatus> deleteRecruit(User user, @PathVariable("id") long recSeq,
+			@RequestBody ProjectRecruit recruit) {
+		if (user.getUserId() == null) {
+			System.out.println("실패");
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		System.out.println("성공");
 		projectRecruitService.deleteProjectRecruit(recruit);
-		return "forward:getProjectRecruitList";
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	// 프로젝트 제거 트래커 고려해서 수정필요
+	@DeleteMapping("/deletepj/{id}")
+	public ResponseEntity<HttpStatus> deleteProject(User user, @PathVariable("id") long id, Project project) {
+		if (user.getUserId() == null) {
+			System.out.println("실패");
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		projectService.deleteProject(project);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
--- src/main/java/com/gaepom/domain/ProjectRecruit.java
+++ src/main/java/com/gaepom/domain/ProjectRecruit.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,40 +22,35 @@ import lombok.ToString;
 @Getter
 @Setter
 @Entity
-@Table(name="project_recruit")
+@ToString(exclude = "project")
+@Table(name = "project_recruit")
 public class ProjectRecruit {
 
 	@Id
 	@GeneratedValue
 	private Long recSeq;
-	
-	@Column(name="need_num")
+
+	@Column(name = "need_num")
 	private int needNum;
-	
-	@Column(name="need_posi")
+
+	@Column(name = "need_posi")
 	private String needPosi;
-	
+
 	private String location;
-	
+
 	private String preference;
-	
-	@Column(name="rec_status")
+
+	@Column(name = "rec_status")
 	private int recStatus;
-	
-	@Column(name="rec_duration")
+
+	@Column(name = "rec_duration")
 	private String recDuration;
-	
-	@Column(name="rec_date", insertable = false, updatable = false, columnDefinition = "date default sysdate")
+
+	@Column(name = "rec_date", insertable = false, updatable = false, columnDefinition = "date default sysdate")
 	private Date recDate;
-	
-	@OneToOne(mappedBy="recSeq")
+
+	@JsonIgnore
+	@OneToOne(mappedBy = "recSeq")
 	private Project project;
 
-//	@Override
-//	public String toString() {
-//		return "ProjectRecruit [recSeq=" + recSeq + ", needNum=" + needNum + ", needPosi=" + needPosi + ", location="
-//				+ location + ", preference=" + preference + ", recStatus=" + recStatus + ", recDuration=" + recDuration
-//				+ ", recDate=" + recDate + ", project=" + project.getPjSeq() + "]";
-//	}
-	
 }
