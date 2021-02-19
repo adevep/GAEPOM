a4ee86f7b9c164bf5b5a635b249d5c87c60fa376
ajdeve <ajdevlife@gmail.com>
2021-02-19 17:53:02 +0900

    Combined all

--- view/src/views/loginUserView/MyPage.vue
+++ view/src/views/loginUserView/MyPage.vue
@@ -1,48 +1,14 @@
 <template>
   <div class="container">
     <section><mypagecom /></section>
-    <section>
-      <b-collapse :open="false" aria-id="contentIdForA11y1">
-        <template #trigger>
-          <b-button
-            label="내 지원서 보기"
-            type="is-primary"
-            aria-controls="contentIdForA11y1"
-          />
-        </template>
-        <div class="content">
-          <application></application>
-        </div>
-      </b-collapse>
-    </section>
-    <br />
-   
-     <section>
-      <b-collapse :open="false" aria-id="contentIdForA11y1">
-        <template #trigger>
-          <b-button
-            label="내 주최 프로젝트 보기"
-            type="is-primary"
-            aria-controls="contentIdForA11y1"
-          />
-        </template>
-        <div class="content">
-          <my-projects></my-projects>
-        </div>
-      </b-collapse>
-    </section>
-  </div>
+  </div> 
 </template>
 <script>
 import mypagecom from "@/components/loginUserCom/MyPageCom.vue";
-import Application from "@/components/Application.vue";
-import MyProjects from "@/components/MyProjects.vue";
 export default {
   name: "mypage",
   components: {
-    mypagecom,
-    Application,
-    MyProjects
+    mypagecom
   }
 };
 </script>
