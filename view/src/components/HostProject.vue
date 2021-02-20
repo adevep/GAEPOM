a1ed2cd1444079b6a2e7d4c875bb4abc0327b9e7
ajdeve <ajdevlife@gmail.com>
2021-02-21 01:37:24 +0900

    Web design revamp

--- view/src/components/HostProject.vue
+++ view/src/components/HostProject.vue
@@ -4,10 +4,38 @@
     v-slot="{ handleSubmit }"
     autocomplete="off"
   >
-    <div class="container is-max-desktop">
+    <div id="app" class="container is-max-desktop pt-5">
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Jua&display=swap"
+        rel="stylesheet"
+      />
+      <section>
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <h1 class="title ">
+              프로젝트
+            </h1>
+            <h2 class="subtitle centered">
+              새로운 프로젝트를 주최해 보세요. 
+            </h2>
+            <nav
+              class="breadcrumb has-dot-separator is-centered"
+              aria-label="breadcrumbs"
+            >
+              <ul>
+                <li><a href="#">홈페이지</a></li>
+                <li><a href="#">마이페이지</a></li>
+                <li class="is-active">
+                  <a href="#" aria-current="page">프로젝트 주최</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </section>
       <div class="notification is-accent">
-        <h1><strong>프로젝트 모집글 만들기</strong></h1>
-        <section class="mt-6 mb-5">
+        <section>
           <b-field label="프로젝트주제" align="left">
             <BInputWithValidation
               v-model="pjTitle"
@@ -211,8 +239,9 @@
           outlined
           @click="handleSubmit(addProjectRec)"
           position="is-centered"
-          size="is-large"
-          >모집글 생성완료</b-button
+          size="is-medium"
+          class="mt-6"
+          >주최하기</b-button
         >
       </div>
       <div class="container is-max-desktop pt-5"></div>
--- view/src/components/RecruitList.vue
+++ view/src/components/RecruitList.vue
@@ -1,132 +1,174 @@
 <template>
-  <div class="container">
+  <div id="app" class="pt-5">
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Jua&display=swap"
+      rel="stylesheet"
+    />
     <link
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/@mdi/font@5.8.55/css/materialdesignicons.min.css"
     />
+    <section>
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title ">
+            프로젝트 찾기
+          </h1>
+          <h2 class="subtitle centered">
+            넥스트 삼산텍은 내 손에서!
+          </h2>
+          <nav
+            class="breadcrumb has-dot-separator is-centered"
+            aria-label="breadcrumbs"
+          >
+            <ul>
+              <li><a href="#">홈페이지</a></li>
+              <li class="is-active">
+                <a href="#" aria-current="page">프로젝트</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div class="section">
+            <b-field grouped group-multiline class="mb-6">
+            <b-field>
+            <b-dropdown aria-role="list" v-model="selectedLoc">
+              <template #trigger="{ active }">
+                <b-button
+                  :label="selectedLoc"
+                  :icon-right="active ? 'menu-up' : 'menu-down'"
+                  centered
+                />
+              </template>
 
-    <b-field class="pt-4">
-      <b-input
-        placeholder="프로젝트 타이틀 검색..."
-        type="text"
-        v-model="searchValue"
-      ></b-input>
-
-      <b-dropdown class="ml-3" aria-role="list" v-model="selectedLoc">
-        <template #trigger="{ active }">
-          <b-button
-            :label="selectedLoc"
-            type="is-primary"
-            :icon-right="active ? 'menu-up' : 'menu-down'"
-            centered
-          />
-        </template>
-
-        <b-dropdown-item aria-role="listitem" value="지역" selected="selected"
-          >전체</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="서울"
-          >서울</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="경기"
-          >경기</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="인천"
-          >인천</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="대전"
-          >대전</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="세종"
-          >세종</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="충북"
-          >충북</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="충남"
-          >충남</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="강원"
-          >강원</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="부산"
-          >부산</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="울산"
-          >울산</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="대구"
-          >대구</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="경북"
-          >경북</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="경남"
-          >경남</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="광주"
-          >광주</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="전북"
-          >전북</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="전남"
-          >전남</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="제주"
-          >제주</b-dropdown-item
-        >
-      </b-dropdown>
-      <b-dropdown aria-role="list" v-model="selectedCate">
-        <template #trigger="{ active }">
-          <b-button
-            :label="selectedCate"
-            type="is-primary"
-            :icon-right="active ? 'menu-up' : 'menu-down'"
-          />
-        </template>
-        <b-dropdown-item
-          aria-role="listitem"
-          value="카테고리"
-          selected="selected"
-          >전체</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="웹앱"
-          >웹앱</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="모바일앱"
-          >모바일앱</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="데이터사이언스"
-          >데이터사이언스</b-dropdown-item
-        >
-        <b-dropdown-item aria-role="listitem" value="게임개발"
-          >게임개발</b-dropdown-item
-        >
-      </b-dropdown>
-    </b-field>
-
-    <div class="container">
-      <div class="card mt-4 mb-4" v-for="pj in filteredPjs" :key="pj.index">
-        <header class="card-header">
-          <p class="card-header-title">
-            <router-link
-              :to="{ name: 'details', params: { pjSeq: pj.pjSeq } }"
-              >{{ pj.pjTitle }}</router-link
+              <b-dropdown-item
+                aria-role="listitem"
+                value="지역"
+                selected="selected"
+                >전체</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="서울"
+                >서울</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="경기"
+                >경기</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="인천"
+                >인천</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="대전"
+                >대전</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="세종"
+                >세종</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="충북"
+                >충북</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="충남"
+                >충남</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="강원"
+                >강원</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="부산"
+                >부산</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="울산"
+                >울산</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="대구"
+                >대구</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="경북"
+                >경북</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="경남"
+                >경남</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="광주"
+                >광주</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="전북"
+                >전북</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="전남"
+                >전남</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="제주"
+                >제주</b-dropdown-item
+              >
+            </b-dropdown>
+            <b-dropdown aria-role="list" v-model="selectedCate">
+              <template #trigger="{ active }">
+                <b-button
+                  :label="selectedCate"
+                  :icon-right="active ? 'menu-up' : 'menu-down'"
+                />
+              </template>
+              <b-dropdown-item
+                aria-role="listitem"
+                value="카테고리"
+                selected="selected"
+                >전체</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="웹앱"
+                >웹앱</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="모바일앱"
+                >모바일앱</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="데이터사이언스"
+                >데이터사이언스</b-dropdown-item
+              >
+              <b-dropdown-item aria-role="listitem" value="게임개발"
+                >게임개발</b-dropdown-item
+              >
+            </b-dropdown>
+          </b-field>
+          <b-field class="ml-2">
+            <b-input
+              placeholder="프로젝트 타이틀 검색..."
+              type="text"
+              v-model="searchValue"
+            ></b-input>
+          </b-field>
+</b-field>
+          <div class="container">
+            <div
+              class="card mt-4 mb-4"
+              v-for="pj in filteredPjs"
+              :key="pj.index"
             >
-          </p>
-        </header>
-        <div class="card-content">
-          <div class="content">
-            {{ pj.pjDescription }}
-            <b-tag type="is-primary" class="mr-2">{{ pj.location }} </b-tag>
-            <b-tag type="is-primary">{{ pj.pjCategory }}</b-tag>
-            <br />
-            <footer class="card-footer"></footer>
+              <header class="card-header">
+                <p class="card-header-title">
+                  <router-link
+                    :to="{ name: 'details', params: { pjSeq: pj.pjSeq } }"
+                    >{{ pj.pjTitle }}</router-link
+                  >
+                </p>
+              </header>
+              <div class="card-content">
+                <div class="content">
+                  {{ pj.pjDescription }}
+                  <b-tag type="is-primary" class="mr-2"
+                    >{{ pj.location }}
+                  </b-tag>
+                  <b-tag type="is-primary">{{ pj.pjCategory }}</b-tag>
+                  <br />
+                  <footer class="card-footer"></footer>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 <script>
